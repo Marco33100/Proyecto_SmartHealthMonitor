e5921c49c6c954eba0097a7a730e964c1a51467f
Marco <martinezramirezm82@gmail.com>
2026-06-10 17:30:46 -0600

    feat: cambios que deberian de ir en otra rama

--- app/src/main/java/mx/utng/mamr/smarthealthmonitor/ui/viewmodel/DashboardViewModel.kt
+++ app/src/main/java/mx/utng/mamr/smarthealthmonitor/ui/viewmodel/DashboardViewModel.kt
@@ -3,27 +3,29 @@ package mx.utng.mamr.smarthealthmonitor.ui.viewmodel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.*
+import mx.utng.mamr.smarthealthmonitor.data.db.LecturaFC
 import mx.utng.mamr.smarthealthmonitor.data.models.SmartHealthRepository
 import mx.utng.mamr.smarthealthmonitor.data.models.MockData
 
 class DashboardViewModel : ViewModel() {
 
-    // FC: viene del wearable real vía Repository.
-    // Si es 0 (sin dato aún), usar valor simulado.
     val fc: StateFlow<Int> = SmartHealthRepository.fcFlow
         .map { if (it == 0) MockData.fcActual else it }
-        .stateIn(
-            scope          = viewModelScope,
-            started        = SharingStarted.WhileSubscribed(5_000),
-            initialValue   = MockData.fcActual
-        )
+        .stateIn(viewModelScope,
+            SharingStarted.WhileSubscribed(5_000), MockData.fcActual)
 
     val pasos: StateFlow<Int> = SmartHealthRepository.pasosFlow
         .map { if (it == 0) MockData.pasosActual else it }
-        .stateIn(
-            scope        = viewModelScope,
-            started      = SharingStarted.WhileSubscribed(5_000),
-            initialValue = MockData.pasosActual
-        )
-    val historial = MockData.historialFC  // TODO S7: Room
+        .stateIn(viewModelScope,
+            SharingStarted.WhileSubscribed(5_000), MockData.pasosActual)
+
+    // ← NUEVO: historial desde Room (Flow reactivo)
+    val historial: StateFlow<List<LecturaFC>> =
+        SmartHealthRepository.obtenerHistorial()
+            .stateIn(
+                scope        = viewModelScope,
+                started      = SharingStarted.WhileSubscribed(5_000),
+                initialValue = emptyList()
+            )
 }
+
