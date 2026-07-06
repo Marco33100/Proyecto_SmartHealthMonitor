28827aaa22ea5f4b2e341bf3319aa20a264107ea
Marco <martinezramirezm82@gmail.com>
2026-07-06 17:20:43 -0600

    chore: add Android TV module with Leanback setup, MainActivity and Theme

---
+++ tv/src/main/java/mx/utng/mamr/smarthealthmonitor/tv/MainFragment.kt
@@ -0,0 +1,9 @@
+package mx.utng.mamr.smarthealthmonitor.tv
+
+import androidx.leanback.app.BrowseSupportFragment
+
+/**
+ * Placeholder MainFragment that extends BrowseSupportFragment.
+ * This ensures MainActivity compiles successfully.
+ */
+class MainFragment : BrowseSupportFragment()
