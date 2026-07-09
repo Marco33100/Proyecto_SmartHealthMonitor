a3142e7bff37802c51867fb665b152dcda5ec8c6
Marco <martinezramirezm82@gmail.com>
2026-07-09 01:41:43 -0600

    feat(cast): add CastOptionsProvider and AndroidManifest meta-data

---
+++ app/src/main/java/mx/utng/mamr/smarthealthmonitor/cast/CastOptionsProvider.kt
@@ -0,0 +1,17 @@
+package mx.utng.mamr.smarthealthmonitor.cast
+
+import android.content.Context
+import com.google.android.gms.cast.CastMediaControlIntent
+import com.google.android.gms.cast.framework.*
+
+class CastOptionsProvider : OptionsProvider {
+    override fun getCastOptions(ctx: Context): CastOptions =
+        CastOptions.Builder()
+            .setReceiverApplicationId(
+                CastMediaControlIntent.DEFAULT_MEDIA_RECEIVER_APPLICATION_ID
+            )
+            .build()
+
+    override fun getAdditionalSessionProviders(ctx: Context): List<SessionProvider> =
+        emptyList()
+}
