53e19f028d05c862f7dd205e03ce8fe1febdf02d
Marco33100 <martinezramirezm82@gmail.com>
2026-07-12 14:05:07 -0600

    fix(db): increment database version to 3 to resolve Room schema integrity error

--- shared/src/main/java/mx/utng/mamr/smarthealthmonitor/data/db/SmartHealthDB.kt
+++ shared/src/main/java/mx/utng/mamr/smarthealthmonitor/data/db/SmartHealthDB.kt
@@ -5,7 +5,7 @@ import androidx.room.*
 
 @Database(
     entities = [LecturaFC::class],
-    version  = 2,
+    version  = 3,
     exportSchema = false  // true en producción para migraciones
 )
 abstract class SmartHealthDB : RoomDatabase() {
