80b511026ef11328f6d52329c660801d517d9b88
Marco33100 <martinezramirezm82@gmail.com>
2026-07-12 10:54:10 -0600

    feat(mqtt): add MqttConfig and message data classes

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -11,11 +11,23 @@ android {
     defaultConfig {
         minSdk = 26
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        
+        val properties = java.util.Properties()
+        val localPropertiesFile = project.rootProject.file("local.properties")
+        if (localPropertiesFile.exists()) {
+            properties.load(localPropertiesFile.inputStream())
+        }
+        buildConfigField("String", "MQTT_BROKER_URL", "\"${properties.getProperty("mqtt.broker_url") ?: ""}\"")
+        buildConfigField("String", "MQTT_USERNAME", "\"${properties.getProperty("mqtt.username") ?: ""}\"")
+        buildConfigField("String", "MQTT_PASSWORD", "\"${properties.getProperty("mqtt.password") ?: ""}\"")
     }
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+    }
+    buildFeatures {
+        buildConfig = true
     }
 }
 
